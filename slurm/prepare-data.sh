f56ea814faefabcdd12749f6408b0a3b998369ce
Trevor Vannoy <trevorvannoy@montana.edu>
2024-12-30 12:45:56 -0700

    remove unused code

--- slurm/prepare-data.sh
+++ slurm/prepare-data.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-output=`sbatch combineData.slurm`
-jobid=`echo "${output}" | cut -d' ' -f4`
-
-output=`sbatch --dependency=afterok:"${jobid}" preprocess.slurm`
-jobid=`echo "${output}" | cut -d' ' -f4`
-
-output=`sbatch --dependency=afterok:"${jobid}" splitData.slurm`
+sbatch splitData.slurm
 
