8ca697bc7546b4735a446fa20f97e073276c24fd
Trevor Vannoy <trevorvannoy@montana.edu>
2024-12-31 09:04:25 -0700

    remove image results section
    We aren't calculating image results because each image has a drone
    in it...

--- supervised-classification/training/data-sampling/writeGridSearchResultsToTxtFile.m
+++ supervised-classification/training/data-sampling/writeGridSearchResultsToTxtFile.m
@@ -15,17 +15,6 @@ dataSetup;
 % Open output file
 fd = fopen(samplingResultsDir + filesep + classifierName + "Results.txt", "w");
 
-
-% Compute the image-based results
-imageResults.Confusion = imageConfusion(rowResults.PredictedLabels,rowResults.TrueLabels);
-
-[a, p, r, f2, ~, mcc] = analyzeConfusion(imageResults.Confusion);
-imageResults.Accuracy = a;
-imageResults.Precision = p;
-imageResults.Recall = r;
-imageResults.F2 = f2;
-imageResults.MCC = mcc;
-
 % Write the classification results to the output text file
 fprintf(fd,"Row results:\n");
 
@@ -41,24 +30,10 @@ fprintf(fd,"Accuracy = %.3f\n",rowResults.Accuracy);
 
 fprintf(fd,"\n\n");
 
-fprintf(fd,"Image results:\n");
-
-fprintf(fd,"Confusion\n");
-fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(1,1),imageResults.Confusion(1,2));
-fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(2,1),imageResults.Confusion(2,2));
-
-fprintf(fd,"Precision = %.3f\n",imageResults.Precision);
-fprintf(fd,"Recall = %.3f\n",imageResults.Recall);
-fprintf(fd,"F2 = %.3f\n",imageResults.F2);
-fprintf(fd,"MCC = %.3f\n",imageResults.MCC);
-fprintf(fd,"Accuracy = %.3f\n",imageResults.Accuracy);
-
-fprintf(fd,"\n\n");
-
 fprintf(fd,"Sampling parameters:\n");
 
 fprintf(fd,"Undersample ratio = %f\n",samplingParams.UndersampleRatio);
-fprintf(fd,"# synthetic insects = %f\n",samplingParams.NOversample);
+fprintf(fd,"# synthetic  = %f\n",samplingParams.NOversample);
 
 fprintf(fd,"\n\n");
 
